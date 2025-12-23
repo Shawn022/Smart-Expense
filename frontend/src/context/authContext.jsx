6d3e67c0f3f5c690e07843cc8df3478f3e0c180c
Shawn022 <shawn58391@gmail.com>
2025-12-23 23:59:31 +0530

    bug

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -14,6 +14,7 @@ export const AuthProvider =({children}) => {
                 setUser(data);
             }catch{
                 setUser(null);
+                setLoading(false);
             }finally{
                 setLoading(false);
             }
