65b93886ba4807b3e83a229110b69fe3e5dd781a
Shawn022 <shawn58391@gmail.com>
2025-12-23 23:38:33 +0530

    123

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/protectedRoute.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/dashboard.jsx";
 
 function App() {
-  
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-
-        <Route path="/dashboard" element={
+        <Route path="/" element={
           <ProtectedRoute>
-            <Dashboard/>
+            <Dashboard />
           </ProtectedRoute>
         } />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
       </Routes>
     </BrowserRouter>
   )
