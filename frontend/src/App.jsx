07bd8458ef4418aee1c12f99714f1167ba2f903e
Shawn022 <shawn58391@gmail.com>
2025-12-23 23:22:15 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter,Routes,Route } from "react-router-dom";
 import ProtectedRoute from "./components/protectedRoute.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
-import Dashboard from "./pages/.jsx";
+import Dashboard from "./pages/dashboard.jsx";
 
 function App() {
   
